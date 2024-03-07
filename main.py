0a061c425400bbb2eaebe05f9280bdd2ca8792bd
ligma <zozworekadam@gmail.com>
2024-03-07 10:35:49 +0100

    feat: calculator

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+import modul
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    while True:
+        print("____KALKULATOR____")
+        a = int(input("Podaj liczbę 1\n"))
+        b = int(input("Podaj liczbę 2\n"))
+        wybor = input("Co chcesz zrobić? \n 1=dodawanie \n 2=odejmowanie\n 3=mnozenie \n 4=dzielenie\n coś innego = wyjdź \n")
+        match wybor:
+            case "1":
+                print(f"{a} + {b} = {modul.dodawanie(a,b)}")
+                continue;
+            case "2":
+                print(f"{a} - {b} = {modul.odejmowanie(a,b)}")
+                continue;
+            case "3":
+                print(f"{a} * {b} = {modul.mnozenie(a,b)}")
+                continue;
+            case "4":
+                print(f"{a} / {b} = {modul.dzielenie(a,b)}")
+                continue;
+            case _:
+                print("\nWychodzenie...\n")
+                quit()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
