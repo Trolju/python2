307f1f695bcaabce09325fdcc13b3ef3ffa22928
RedDuo <goskar0808@gmail.com>
2024-03-07 10:25:21 +0100

    moduly do kalkulatora

---
+++ modul.py
@@ -0,0 +1,11 @@
+def dodawanie(a, b):
+    return a + b
+def odejmowanie(a, b):
+    return a - b
+def mnozenie(a, b):
+    return a * b
+def dzielenie(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "nie mozna dzielic przez 0"
